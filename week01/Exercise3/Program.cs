0e772c76a9793a04ec5ae046f7a05bb853f6e2ee
Dieuveille <mabdieuveille@gmail.com>
2026-01-18 14:58:10 +0200

    week-01-finished-exercise-3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+       
+        string keepOnPlaying = "yes";
+
+        // while loop
+        while (keepOnPlaying == "yes")
+        {
+             // Guessing Number
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            Console.WriteLine($"The current magic number: {magicNumber}");
+
+            int guess = -1;
+            int guessCount = 0;
+
+             while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                    guessCount += 1;
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                    guessCount += 1;
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it with {guessCount} guesses!");
+                    Console.Write("Would you want to play again (Yes/No)?");
+                    keepOnPlaying = Console.ReadLine().ToLower().Trim();
+                }
+            }
+        }
+       
+
     }
 }
