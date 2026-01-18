e8fde8fddfdadbf8694e9707840d6750148c21d6
Dieuveille <mabdieuveille@gmail.com>
2026-01-18 14:13:44 +0200

    week-01-exercise-2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,79 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+        int lastDigit = percent % 10;
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+
+            if (lastDigit < 3)
+            {
+                letter += "-";
+            }
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+            if (lastDigit >= 7)
+            {
+                letter += "+";
+            }
+            
+            if (lastDigit < 3)
+            {
+                letter += "-";
+            }
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+            if (lastDigit >= 7)
+            {
+                letter += "+";
+            }
+            
+            if (lastDigit < 3)
+            {
+                letter += "-";
+            }
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+            if (lastDigit >= 7)
+            {
+                letter += "+";
+            }
+            
+            if (lastDigit < 3)
+            {
+                letter += "-";
+            }
+        }
+        else
+        {
+            letter = "F";
+            if (lastDigit < 3)
+            {
+                letter += "-";
+            }
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
