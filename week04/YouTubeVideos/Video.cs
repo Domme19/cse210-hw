ca7dfe1c7304ba10ff813306ca8bee5921cbcfdb
Dieuveille <mabdieuveille@gmail.com>
2026-02-07 16:53:16 +0200

    video id created

--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -1,8 +1,37 @@
+using System.Net.Http.Headers;
+using System.Net.NetworkInformation;
+using System.Reflection;
+
 class Video
 {
-      private string _id;
       private string _title;
       private string _author;
-      private string _length;
-      
+      private int _length;
+
+      private static int suffix = 1100;
+      private string currentVideoId = "";
+
+
+    //   an internal function that creates the id;
+    private string createRandomId()
+    {
+        // increment
+        suffix += 1;
+        string videoId = "VD" + suffix.ToString();
+        return videoId;
+    }
+
+      public Video (string title, string author, int length)
+    {
+        _title = title;
+        _author = author;
+        _length = length;
+        currentVideoId = createRandomId();
+    }
+
+
+    public string GetVideoId()
+    {
+        return currentVideoId;
+    }
 }
