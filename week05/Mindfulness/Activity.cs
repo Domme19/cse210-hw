1b55884478d333b2030563d9ca02ea3cd11ca9a8
Dieuveille <mabdieuveille@gmail.com>
2026-02-14 14:02:09 +0200

    setting up Activity class

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,30 @@
+using System.Dynamic;
+
+public class Activity
+{
+    private string _name;
+    private string _description;
+    private int _duration;
+
+    public Activity(string name, string description, int duration)
+    {
+        _name = name;
+        _description = description;
+        _duration = duration;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public string GetDescription()
+    {
+        return _description;
+    }
+
+    public int GetDuration()
+    {
+        return _duration;
+    }
+}
--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        Console.WriteLine("Let us start working"); 
+        // TODO: Create Class Activity
     }
 }
