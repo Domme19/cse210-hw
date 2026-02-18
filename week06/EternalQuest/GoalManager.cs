6dc3039e7c2ea7ccc2f7df8cd97df82001e0e19e
Dieuveille <mabdieuveille@gmail.com>
2026-02-18 10:46:56 +0200

    Goal manager class added

---
+++ week06/EternalQuest/GoalManager.cs
@@ -0,0 +1,11 @@
+public class GoalManager
+{
+    private List<Goal> _goals;
+    private int _score;
+
+    public GoalManager(List<Goal> goals, int score)
+    {
+        _goals = goals;
+        _score = score; 
+    }
+}
