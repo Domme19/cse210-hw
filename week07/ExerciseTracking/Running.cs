db3e254845163c84539b509ce0f7f42433567923
Dieuveille <mabdieuveille@gmail.com>
2026-02-19 20:40:33 +0200

    running class added

---
+++ week07/ExerciseTracking/Running.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics.Contracts;
+
+public class Running : Activity
+{
+    private double _distance; // In miles
+
+    public Running(string date, int minutes, double distance) : base(date, minutes)
+    {
+        _distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return _distance;
+    }
+
+    public override double GetSpeed()
+    {
+        double distance = GetDistance();
+        int minutes = GetMinutes();
+        double division = distance / minutes;
+        return division * 60; 
+    }
+
+    public override double GetPace()
+    {
+        int minutes = GetMinutes();
+        double distance = GetDistance();
+        return minutes / distance;
+    }
+}
